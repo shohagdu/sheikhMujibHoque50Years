7ace30ad070b56b42978166daac822c567143f9c
omarshohagdu <omarfaruk@du.ac.bd>
2022-06-13 01:00:00 +0600

    update ssl commerce update

--- resources/views/admin/registerApplicant/waitingForPayment.blade.php
+++ resources/views/admin/registerApplicant/waitingForPayment.blade.php
@@ -238,7 +238,7 @@
                                                 @if(!empty($applicantInfo->paidStatus) &&
                                                 $applicantInfo->paidStatus==1 &&
                                     $applicantInfo->netAmount > 0 )
-                                                    <button class="your-button-class" id="sslczPayBtn"
+                                                    <button type="button" class="your-button-class" id="sslczPayBtn"
                                                             token="if you have any token validation"
                                                             postdata="your javascript arrays or objects which requires in backend"
                                                             order="If you already have the transaction generated for current order"
@@ -274,6 +274,7 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
             integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
             crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
             integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
             crossorigin="anonymous"></script>
