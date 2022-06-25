f8b7671171842014fa3f06b22bcc94b932853fa5
omarshohagdu <omarfaruk@du.ac.bd>
2022-06-25 13:03:58 +0600

    documention issue fixed

--- resources/views/home/registrationProcess.blade.php
+++ resources/views/home/registrationProcess.blade.php
@@ -11,48 +11,56 @@
             <li>১। রেজিস্ট্রেশন।  <a href="{{ url('/signUp') }}"   target="_blank">লিংক</a> </li>
             <li>২। মোবাইল নম্বর ও পাসওয়ার্ড দিয়ে লগইন  করতে হবে। <a href="{{ url('/admin') }}" target="_blank">লিংক</a>
             </li>
-            <li>৩। লগইন করার পর <span style="color:blue"> Confirm Registration এবং  রেজিস্ট্রেশন ফি প্রদান</span> এর  মাধ্যমে  রেজিস্ট্রেশন   প্রক্রিয়া  সম্পন্ন করা</li>
+            <li>৩। লগইন করার পর <span style="color:blue"> Confirm Registration এবং  রেজিস্ট্রেশন ফি প্রদান</span> এর
+                মাধ্যমে  রেজিস্ট্রেশন   প্রক্রিয়া  সম্পন্ন করতে হবে।</li>
         </ul>
         <div class="col-sm-8 text-center" style="margin-top: 50px"> <h4>---গাইডলাইন---</h4> </div>
         <div class="col-sm-8">
             <ul>
                 <li>
-                   <h5 class="guideline"> ১। প্রথমে রেজিস্ট্রেশন অপশনে ক্লিক করে রেজিস্ট্রেশন করতে হবে।
-                    রেজিস্ট্রেশন ফর্ম নিচে দেওয়া হল-</h5>
+                   <h5 class="guideline"> ১। প্রথমে রেজিস্ট্রেশন অপশনে ক্লিক করে রেজিস্ট্রেশন করতে হবে।</h5>
+                    <h6 class="guidelineSub"> রেজিস্ট্রেশন ফর্ম নিচে দেওয়া হল-</h6>
                     <img src="{{ asset('public/web/documents/register.png') }}" class="img-thumbnail" style="width: 100%">
                 </li>
                 <li>
                     <h5 class="guideline"> ২। রেজিস্ট্রেশন সম্পন্ন হওয়ার পর লগইন পেইজ থেকে লগইন  করতে হবে।</h5>
 
-                    লগইন ফর্ম নিচে দেওয়া হল-
+                    <h6 class="guidelineSub">লগইন ফর্ম নিচে দেওয়া হল-</h6>
                     <img src="{{ asset('public/web/documents/login.png') }}" class="img-thumbnail" style="width: 100%">
                 </li>
                 <li>
                     <h5 class="guideline">৩. Confirm Registration এবং  রেজিস্ট্রেশন ফি প্রদান</span> এর  মাধ্যমে  রেজিস্ট্রেশন   প্রক্রিয়া
                         সম্পন্ন করা। এখানে চারটি ধাপে পেমেন্ট প্রসেস সম্পন্ন করতে হবে। </h5>
                     <br/>
-                    <h6>i) লগইন সম্পন্ন হওয়ার পর ড্যাশবোর্ড থেকে Confirm registration বাটনে ক্লিক করতে হবে।</h6>
+                    <h6 class="guidelineSub">i) লগইন সম্পন্ন হওয়ার পর ড্যাশবোর্ড থেকে Confirm registration বাটনে ক্লিক করতে হবে।</h6>
                     <img src="{{ asset('public/web/documents/deshobard.png') }}" class="img-thumbnail" style="width:
                     100%">
                     <br/>   <br/>
-                    <h6>ii) রেজিস্ট্রেশন সম্পর্কিত কিছু তথ্য দিতে দিয়ে Submit বাটনে ক্লিক করতে হবে।</h6>
+                    <h6 class="guidelineSub">ii) রেজিস্ট্রেশন সম্পর্কিত কিছু তথ্য দিতে দিয়ে Submit বাটনে ক্লিক করতে হবে।</h6>
                     <img src="{{ asset('public/web/documents/dashboard2.png') }}" class="img-thumbnail" style="width:
                     100%">
                     <br/>   <br/>
-                    <h6>iii) Submit বাটনে ক্লিক করার পর আপনার প্রদানকৃত তথ্য সমূহ আবার যাচাই করার পর Pay Now বাটনে ক্লিক
+                    <h6 class="guidelineSub">iii) Submit বাটনে ক্লিক করার পর আপনার প্রদানকৃত তথ্য সমূহ আবার যাচাই করার পর Pay Now বাটনে
+                        ক্লিক
                         করে
                         পেমেন্ট গেটওয়ের মাধ্যমে অনলাইনে টাকা প্রদান করতে হবে</h6>
                     <img src="{{ asset('public/web/documents/dashboard3.png') }}" class="img-thumbnail" style="width:
                     100%">
                     <br/>   <br/>
-                    <h6>iv) Pay Now বাটনে ক্লিক করে আপনার CARDS, MOBILE BANKING, NET BANKING এর মাধ্যমে টাকা প্রদান করতে
+                    <h6 class="guidelineSub">iv) Pay Now বাটনে ক্লিক করে আপনার CARDS, MOBILE BANKING, NET BANKING এর মাধ্যমে টাকা প্রদান
+                        করতে
                         পারবেন।</h6>
                     <img src="{{ asset('public/web/documents/dashboard4.png') }}" class="img-thumbnail" style="width:
                     100%">
-
-
-
                 </li>
+                <li style="color:blue;padding-top: 20px;text-align: center;">**** পেমেন্ট করার পর  আপনার Register কৃত
+                    মোবাইল নম্বরে
+                    একটি
+                    SMS
+                    পাবেন। **** </li>
+
+
+                <li style="color:green;padding-top: 20px;text-align: center;">**** ধন্যবাদ **** </li>
 
             </ul>
         </div>
@@ -60,9 +68,19 @@
     </div>
     <style>
         .listItem ul li{
-            font-size: 16px;
+            font-size: 18px;
             padding: 3px 0px;
             list-style-type: none;
         }
+        .guideline{
+            margin-top:20px;
+            line-height: 30px;
+        }
+        .guidelineSub{
+            font-weight: bold !important;
+            line-height: 27px;
+            margin-top:20px;
+        }
+
     </style>
 @endsection
